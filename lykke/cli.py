a10cf89fa2ac4797c5593125390b6a7ec3f84403
Paul Pfeiffer <pfeifferpaul90@gmail.com>
2017-12-03 13:30:44 +0100

    * extract api key from config file * retrieve balance

--- lykke/cli.py
+++ lykke/cli.py
@@ -23,21 +23,29 @@ from inspect import getmembers, isclass
 
 from docopt import docopt
 
+from lykke.commands.services.config_service import ConfigService
 from . import __version__ as VERSION
+
+
+class LykkeCliError(BaseException):
+    pass
 
 
 def main():
     """Main CLI entrypoint."""
     import lykke.commands
     options = docopt(__doc__, version=VERSION)
-
-    # Here we'll try to dynamically match the command the user is trying to run
-    # with a pre-defined command class we've already created.
-    # Note that the below procedure assumes that the command files only hold two classes the base class and the command class, so that importing other classes destroys the scheme.
-    for (k, v) in options.items():
-        if hasattr(lykke.commands, k) and v:
-            module = getattr(lykke.commands, k)
-            lykke.commands = getmembers(module, isclass)
-            command = [command[1] for command in lykke.commands if command[0] != 'Base'][0]
-            command = command(options)
-            command.run()
+    try:
+        config_service = ConfigService()
+        # Here we'll try to dynamically match the command the user is trying to run
+        # with a pre-defined command class we've already created.
+        # Note that the below procedure assumes that the command files only hold two classes the base class and the command class, so that importing other classes destroys the scheme.
+        for (k, v) in options.items():
+            if hasattr(lykke.commands, k) and v:
+                module = getattr(lykke.commands, k)
+                lykke.commands = getmembers(module, isclass)
+                command = [command[1] for command in lykke.commands if command[0] != 'Base'][0]
+                command = command(options, config_service=config_service)
+                command.run()
+    except LykkeCliError as err:
+        print "Check out lykke -h"
--- lykke/commands/balance.py
+++ lykke/commands/balance.py
@@ -1,5 +1,5 @@
 """The balance command."""
-
+import services.lykkex_service as ly
 from .base import Base
 
 
@@ -7,4 +7,7 @@ class Balance(Base):
     """Get lykkex balance"""
 
     def run(self):
-        print('Getting balance...')
+        config_service = self.kwargs["config_service"]
+        time_stamp, balances = ly.LykkexService.get_balance(config_service.get_api_key())
+        for balance in balances:
+            print('{}: {:10.4f}'.format(balance["AssetId"], balance["Balance"]))
